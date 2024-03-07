f0eb420e6b21379d4fb9efc6f68ae8959c08a33e
Leonardo Grzybowski <leonardogrz@hotmail.com>
2024-03-07 15:17:33 -0300

    Changes made to app.ts and to matches services and controller

--- app/backend/src/controller/matches.controller.ts
+++ app/backend/src/controller/matches.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import * as jwt from 'jsonwebtoken';
 import matchesService from '../service/matches.service';
-// só pra comittar
+
 const JWT_SECRET = 'jwt_secret';
 const TOKEN_MUST_BE_VALID = 'Token must be a valid token';
 const TOKEN_NOT_FOUND = 'Token not found';
@@ -41,6 +41,28 @@ const getAllMatches = async (req: Request, res: Response) => {
   }
 };
 
+const getAllMatchesAndTeams = async (req: Request, res: Response) => {
+  const auth = req.headers.authorization;
+  if (!auth) {
+    return res.status(401).json({ message: TOKEN_NOT_FOUND });
+  }
+
+  validateToken(req, res);
+
+  const authToken = auth.split(' ')[1];
+  try {
+    jwt.verify(authToken, JWT_SECRET);
+    const matches = await matchesService.getMatchesAndTeams();
+    if (!matches) {
+      return res.status(401).json({ message: TOKEN_MUST_BE_VALID });
+    }
+    return res.status(200).json(matches);
+  } catch (e) {
+    return res.status(401).json({ message: TOKEN_MUST_BE_VALID });
+  }
+};
+
 export default {
   getAllMatches,
+  getAllMatchesAndTeams,
 };
--- app/backend/src/service/matches.service.ts
+++ app/backend/src/service/matches.service.ts
@@ -1,10 +1,39 @@
 import Matches from '../database/models/matches.model';
+import Teams from '../database/models/teams.model';
+import ITeams from '../Interfaces/ITeams';
 
 const getAllMatches = async () => {
   const allMatches = await Matches.findAll();
   return allMatches;
 };
 
+const getAllTeams = async ():Promise<ITeams[]> => {
+  const allTeams = await Teams.findAll();
+  return allTeams;
+};
+
+const getMatchesAndTeams = async ():Promise<any[]> => {
+  const allMatches = await getAllMatches();
+  const allTeams = await getAllTeams();
+
+  const MatchesAndTeams = allMatches.map((match) => {
+    const homeTeam = allTeams.find((team) => team.id === match.homeTeamId);
+    const awayTeam = allTeams.find((team) => team.id === match.awayTeamId);
+    return {
+      id: match.id,
+      homeTeamId: homeTeam?.id,
+      homeTeamGoals: match.homeTeamGoals,
+      awayTeamId: awayTeam?.id,
+      awayTeamGoals: match.awayTeamGoals,
+      inProgress: match.inProgress,
+      homeTeam: { teamName: homeTeam?.teamName },
+      awayTeam: { teamName: awayTeam?.teamName },
+    };
+  });
+  return MatchesAndTeams;
+};
+
 export default {
   getAllMatches,
+  getMatchesAndTeams,
 };
