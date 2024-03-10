590967f1a486eaf5cc7cc57e4d42c152eaa4d4e9
Leonardo Grzybowski <leonardogrz@hotmail.com>
2024-03-10 19:36:34 -0300

    Increasing coverage

--- app/backend/src/tests/matches.controller.test.ts
+++ app/backend/src/tests/matches.controller.test.ts
@@ -7,35 +7,52 @@ import matchesController from '../controller/matches.controller';
 import { getAllMatchesAndTeamsStubReturn } from './allMocks';
 
 describe('MATCHES CONTROLLER', () => {
-  describe('Tests the getAllMatchesAndTeams function', () => {
+  describe('Tests the getInProgressMatches function', () => {
     it('Should return all matches and teams', async () => {
-      const getMatchesAndTeamsStub: any = sinon.stub(matchesService, 'getMatchesAndTeams').resolves(getAllMatchesAndTeamsStubReturn);
+      const getMatchesAndTeamsStub: any = sinon.stub(matchesService, 'getInProgressMatches').resolves(getAllMatchesAndTeamsStubReturn);
       const req = {} as unknown as any;
       const res = {
         status: sinon.stub().returnsThis(),
         json: sinon.stub(),
       } as unknown as any;
 
-      await matchesController.getAllMatchesAndTeams(req, res);
+      await matchesController.getInProgressMatches(req, res);
 
       expect(res.status.calledWith(200)).to.be.true;
       expect(res.json.calledWith(getAllMatchesAndTeamsStubReturn)).to.be.true;
       getMatchesAndTeamsStub.restore();
     });
 
-    it('Should return an error message if getMatchesAndTeams throws an error', async () => {
-      const getMatchesAndTeamsStub: any = sinon.stub(matchesService, 'getMatchesAndTeams').throws(new Error('Invalid token'));
+    it('Should return an error message if getInProgressMatches throws an error', async () => {
+      const getMatchesAndTeamsStub: any = sinon.stub(matchesService, 'getInProgressMatches').throws(new Error('Invalid token'));
       const req = {} as unknown as any;
       const res = {
         status: sinon.stub().returnsThis(),
         json: sinon.stub(),
       } as unknown as any;
 
-      await matchesController.getAllMatchesAndTeams(req, res);
+      await matchesController.getInProgressMatches(req, res);
 
       expect(res.status.calledWith(401)).to.be.true;
       expect(res.json.calledWith({ message: 'Token must be a valid token' })).to.be.true;
       getMatchesAndTeamsStub.restore();
     });
+
+    it('Should return in progress matches', async () => {
+      const getInProgressMatchesStub: any = sinon.stub(matchesService, 'getInProgressMatches').resolves(getAllMatchesAndTeamsStubReturn);
+      const req = { query: { inProgress: 'true' } } as unknown as any;
+      const res = {
+        status: sinon.stub().returnsThis(),
+        json: sinon.stub(),
+      } as unknown as any;
+
+      await matchesController.getInProgressMatches(req, res);
+
+      expect(res.status.calledWith(200)).to.be.true;
+      expect(res.json.calledWith(getAllMatchesAndTeamsStubReturn)).to.be.true;
+      getInProgressMatchesStub.restore();
+    });
   });
+
+  
 });
